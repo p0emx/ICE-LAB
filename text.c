ef50ee9ea16bd5bd35e3f5c88ea78f94f9d04a45
ohm <contact.kanatip@gmail.com>
2026-09-23 14:28:25 +0700

    new loop

--- text.c
+++ text.c
@@ -2,5 +2,10 @@
 
 int main(){
     printf("mint na hee");
-    printf("mint and butter");
+    printf("mint and butter\n");
+    int a = 2;
+    for(;a<5;a++){
+        printf("mint ma ao rai\n");
+    }
+    return 0;
 }
